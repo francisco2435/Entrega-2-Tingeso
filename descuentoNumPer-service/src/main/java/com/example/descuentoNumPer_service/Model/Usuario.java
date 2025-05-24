9dff34e27f67333041668a0640a4f55ca050ae32
francisco2435 <francisco.riquelme.n@usach.cl>
2025-05-24 13:07:34 -0400

    Modificacion microservicio de descuento de numero de personas

--- descuentoNumPer-service/src/main/java/com/example/descuentoNumPer_service/Model/Usuario.java
+++ descuentoNumPer-service/src/main/java/com/example/descuentoNumPer_service/Model/Usuario.java
@@ -1,14 +1,8 @@
 package com.example.descuentoNumPer_service.Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class Usuario {
     private Long id;
     public String rut;
@@ -18,4 +12,81 @@ public class Usuario {
     public String rol;
     public String contrasenia;
     public LocalDate fechaNacimiento;
+
+    public Usuario() {
+    }
+
+    public Usuario(String rut, String nombre, String correo, String telefono, String rol, String contrasenia, LocalDate fechaNacimiento) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.rol = rol;
+        this.contrasenia = contrasenia;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }
